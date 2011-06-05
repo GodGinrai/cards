f6112790bfcdc42535aaef93cfa05c361775b1e1
GodGinrai <GodJinrai@gmail.com>
2011-06-05 01:03:22 -0400

    Dynamic "doo's"

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
 <H1>IT'S TIME TO</H1>
 <BR><BR><BR><BR><BR><BR><BR>
 <H2>Doodoodoodoo</H2>
-<H1>DoodoodoodooDuel!</H1>
+<H1>Doo
+<?php
+for ($i = 0; $i < $doo; $i++) {
+	print "doo";
+}
+?>Duel!</H1>
 </BODY>
