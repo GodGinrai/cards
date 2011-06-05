f12d7649a91f10f73a8a458efd904dad650d0b91
GodGinrai <GodJinrai@gmail.com>
2011-06-04 23:54:23 -0400

    Added awesome Index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<HEAD>
+<TITLE>CARD GAMES ON MOTORCYCLES!!!!!!</TITLE>
+</HEAD>
+<BODY>
+<H1>IT'S TIME TO</H1>
+<BR><BR><BR><BR><BR><BR><BR>
+<H2>Doodoodoodoo</H2>
+<H1>DoodoodoodooDuel!</H1>
+</BODY>
